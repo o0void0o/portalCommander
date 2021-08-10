f032cbd085096be9f795bf31cdad21182b3da84f
Etienne <etienne.swanepoel2@gmail.com>
2021-08-10 22:50:36 +0200

    Updated the command to <xxx,xxx,xxx> format.

--- cpu_usage_RGB.py
+++ cpu_usage_RGB.py
@@ -9,14 +9,13 @@ i=0
 while 1:
 	x = psutil.cpu_percent()
 	if x<30:
-		ser.write(bytes('<0,255,0,1>','utf8'))
+		ser.write(bytes('<0,255,0>','utf8'))
 	
 	if x>50 and x<70:
-		ser.write(bytes('<255,255,0,1>','utf8'))
+		ser.write(bytes('<255,255,0>','utf8'))
 
 	if x>70:
-		ser.write(bytes('<255,0,0,1>','utf8'))
+		ser.write(bytes('<255,0,0>','utf8'))
 
 	time.sleep(0.1)
-	
 ser.close()
--- test.py
+++ test.py
@@ -9,13 +9,13 @@ i=0
 while 1:
 	while i<255:
 		
-		ser.write(bytes('<'+str(i)+',0,0,1>','utf8'))
+		ser.write(bytes('<'+str(i)+',0,0>','utf8'))
 		i=i+1
 		time.sleep(0.001)
 	
 	while i>0:
 		
-		ser.write(bytes('<'+str(i)+',0,0,1>','utf8'))
+		ser.write(bytes('<'+str(i)+',0,0>','utf8'))
 		i=i-1
 		time.sleep(0.001)
 
